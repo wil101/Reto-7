27fbe9747a9099e8900ce9bf55e9d600869d353b
Emanuel López <97258179+EmanuelLopezH@users.noreply.github.com>
2022-08-09 15:17:04 -0500

    Subo la clase abstracta y la clase asfalto

---
+++ src/main/java/Carretera/Asfalto.java
@@ -0,0 +1,47 @@
+package Carretera;
+/*
+ * @author emanuel.lopezh
+ */
+public class Asfalto extends TramoGenerico {
+    
+    private double xInit;
+    private double yInit;
+    private double xFinal;
+    private double yFinal;
+    private String proveedor;
+    private int velMax;
+    private boolean adelantar;
+
+    public Asfalto(double xInit, double yInit, double xFinal, double yFinal, String proveedor) {
+        this.xInit = xInit;
+        this.yInit = yInit;
+        this.xFinal = xFinal;
+        this.yFinal = yFinal;
+        this.proveedor = proveedor;
+    }
+
+    public boolean isAdelantar() {
+        return true;
+    }
+
+    public int getVelMax() {
+        return 150;
+    }
+   
+    @Override
+    public double longitud() {
+        double longitud = Math.sqrt(Math.pow((this.xFinal - this.xInit), 2) + Math.pow((yInit - yFinal), 2));
+        return longitud;
+    }
+
+    @Override
+    public double area() {
+        return longitud()*8.0;
+    }
+
+    @Override
+    public double volumen() {
+        return area()*0.25;
+    }
+    
+}
---
+++ src/main/java/Carretera/TramoGenerico.java
@@ -0,0 +1,10 @@
+package Carretera;
+/**
+ * @author emanuel.lopezh
+ */
+public abstract class TramoGenerico {
+    
+    public abstract double longitud();
+    public abstract double area();
+    public abstract double volumen();
+}
